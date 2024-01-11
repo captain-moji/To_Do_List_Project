ba1cffdd463b7e43e4b7a7746e37c46d5f1c49c8
moh-skec <m.shahpoori@ec.iut.ac.ir>
2024-01-11 10:57:35 +0330

    Person heir classes added

---
+++ ToDoListServer/teamperson.h
@@ -0,0 +1,23 @@
+#ifndef TEAMPERSON_H
+#define TEAMPERSON_H
+
+#include "orgperson.h"
+
+class TeamPerson : public OrgPerson
+{
+private:
+    QString per_team_id;
+    bool team_is_admin;
+
+public:
+    TeamPerson();
+    TeamPerson(QString, OrgPerson);
+    TeamPerson(const TeamPerson &);
+    ~TeamPerson();
+    QString teamPerGetId ()const;
+    void teamPerSetID(QString);
+    bool teamPerGetIsAdmin();
+    void teamPerSetIsAdmin();
+};
+
+#endif // TEAMPERSON_H
