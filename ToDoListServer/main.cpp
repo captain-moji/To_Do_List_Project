e897658af0abad534b25a891b2a3d468e4ed5266
unknown <mojtababaranpur@gmail.com>
2024-01-29 20:55:17 +0330

    fix bugs - set windows titles

--- ToDoListServer/main.cpp
+++ ToDoListServer/main.cpp
@@ -7,12 +7,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //FirstPage f;
-    //f.show();
+    FirstPage f;
+    f.setWindowTitle("Login To To Do List M&M Server");
+    f.show();
 
-    ToDoList t;
-    t.setWindowTitle("To Do List M&M");
-    t.show();
+    //ToDoList t;
+    //t.setWindowTitle("To Do List M&M");
+    //t.show();
 
     return a.exec();
 }
