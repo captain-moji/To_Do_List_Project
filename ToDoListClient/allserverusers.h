4aa79fa4ef5f928d9c5d0bbf742a8c1edae67836
unknown <mojtababaranpur@gmail.com>
2024-02-02 00:36:54 +0330

    Client organizationWindow add person cpmpleted

--- ToDoListClient/allserverusers.h
+++ ToDoListClient/allserverusers.h
@@ -26,33 +26,17 @@ class allServerUsers : public QMainWindow
 public:
     explicit allServerUsers(QWidget *parent = nullptr);
     ~allServerUsers();
-    void btn_disable();
+    void typeSetter(QString type);
 
 private slots:
-    void username_setter(QString);
-    void password_setter(QString);
-    void name_setter(QString);
-    void question_setter(QString);
-    void answer_setter(QString);
-    void person_saver();
-
-    void on_all_users_add_user_BTN_clicked();
-
-    void on_all_users_remove_user_BTN_clicked();
-
-    void on_all_persons_tree_itemDoubleClicked(QTreeWidgetItem *item, int column);
-
-    void delete_person_from_server(QString);
-
-    void on_all_users_search_line_edit_textChanged(const QString &arg1);
+    void on_add_to_org_BTN_clicked();
 
 signals:
-    void org_name_signal(QString);
-    void user_selected(QString);
+
+    void name_selected(QString);
 
 private:
-    void search_user();
-    void all_persons_display();
+
     Person temp_person;
     Ui::allServerUsers *ui;
 
