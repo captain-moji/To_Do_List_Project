86ca3fc9f04d95d1894d214dd2caea8e762ec8c4
unknown <mojtababaranpur@gmail.com>
2024-02-03 15:06:23 +0330

    Client TaskWindow Completed

--- ToDoListClient/taskwindow.h
+++ ToDoListClient/taskwindow.h
@@ -3,8 +3,13 @@
 
 #include <QMainWindow>
 #include <task.h>
+#include <QMessageBox>
+#include <QStringList>
+#include <QListWidgetItem>
 
-namespace Ui {
+
+namespace Ui
+{
 class TaskWindow;
 }
 
@@ -16,19 +21,42 @@ public:
     explicit TaskWindow(QWidget *parent = nullptr);
     ~TaskWindow();
     void typeSetter(QString);
-
+    void thisOrgProjectSetter(QString,QString);
+    void ownerListSetter (QStringList teams , QStringList persons);
 private slots:
-
 
     void on_make_new_task_BTN_clicked();
 
+    void on_save_task_BTN_clicked();
+
+    void this_task_maker(Task);
+
+    void owner_name_setter(QString);
+
+    void owner_id_setter(QString);
+
+    void on_teams_list_widget_itemClicked(QListWidgetItem *item);
+
+    void on_persons_list_widget_itemClicked(QListWidgetItem *item);
+
+    void on_Owner_type_combobox_currentTextChanged(const QString &arg1);
+
 signals:
     void new_task_maked(Task);
+    void task_edited(Task);
+
 
 private:
-    Task new_task;
+
+    QString temp_owner_name;
+    QString temp_owner_id;
+
     Task this_task;
+    QString this_org;
+    QString this_project;
     Ui::TaskWindow *ui;
+
+
 };
 
 #endif // TASKWINDOW_H
